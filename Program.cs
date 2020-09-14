6ce9b0a1aafa92eb9e8b4bb3bb1140393b26dbf4
JY1330 <55523921+JY1330@users.noreply.github.com>
2020-09-14 10:04:47 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201987028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CSharp World!\n");
+            Console.WriteLine("이름 : 김재윤\n");
+            Console.WriteLine("학번 : 201987028\n");
+        }
+    }
+}
